b3bfcd94228631c3d6183ecd129a2a0f9155d4f1
hdinceler <55253026+hdinceler@users.noreply.github.com>
2023-08-12 16:10:48 +0300

    araba_kutuphane.h

--- ARABA 2.0/Araba/ARABA/araba_kutuphane.h
+++ ARABA 2.0/Araba/ARABA/araba_kutuphane.h
@@ -11,6 +11,20 @@
   void right_stop();
   void all_stop();
   void motor_test();
+  int mesafe_olc();
+
+
+int mesafe_olc() {
+  
+  digitalWrite(pin_mesafe_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pin_mesafe_trig, LOW);
+
+  unsigned long sure = pulseIn(pin_mesafe_echo, HIGH);
+  int mesafe_cm = sure * 0.034 / 2;
+  
+  return mesafe_cm;
+}
 
   void run_right( signed short speed);
   void run_left(  signed short speed);
@@ -30,59 +44,59 @@
   void right_back(uint8_t speed){
     analogWrite( pin_R1 , speed );
     analogWrite( pin_R2 , 0 );
-    Serial.print("\n  right_back:");
+    Serial.print("--right_back:");
     Serial.print(speed);
   }
 
   void right_forward(uint8_t speed){
     analogWrite( pin_R1 , 0 );
     analogWrite( pin_R2 , speed );
-    Serial.print("\n  right_forward:");
+    Serial.print("--right_forward:");
     Serial.print(speed);
   }
 
   void left_back (uint8_t speed){
     analogWrite( pin_L1 , 0 );
     analogWrite( pin_L2 , speed );
-    Serial.print("\n  left_back:");
+    Serial.print(" --left_back:");
     Serial.print(speed);
   }
 
   void left_forward(uint8_t speed){
     analogWrite( pin_L1 , speed );
     analogWrite( pin_L2 , 0 );
-    Serial.print("\n  left_forward:");
+    Serial.print("--left_forward:");
     Serial.print(speed);
   }
 
   void left_stop(){
     analogWrite( pin_L1 , 0 );
     analogWrite( pin_L2 , 0 );
-    Serial.print("\n  left_stop");
+    Serial.print("--left_stop");
   }
   void right_stop(){
     analogWrite( pin_R1 , 0 );
     analogWrite( pin_R2 , 0 );
-    Serial.print("\n  right_stop");
+    Serial.print("--right_stop");
   }
 
   void turn_right(uint8_t speed){
     right_back(speed);
     left_forward(speed);
-    Serial.print("\n  turn_right:");
+    Serial.print("--turn_right:");
     Serial.print(speed);
   }
   void turn_left(uint8_t speed){
     right_forward(speed);
     left_back(speed);
-    Serial.print("\n turn_left:");
+    Serial.print("--turn_left:");
     Serial.print(speed);
   }
 
   void all_stop(){
     right_stop();
     left_stop();  
-    Serial.print("\n  all_stop");
+    Serial.print("--  all_stop");
   }
   void motor_test(){
     right_forward(255);
