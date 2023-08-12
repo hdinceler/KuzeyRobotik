019e7f3b5434763fcd2c9c1879d36966f6d7cf70
hdinceler <55253026+hdinceler@users.noreply.github.com>
2023-08-12 19:05:17 +0300

    araba_kutuphane.h

--- ARABA 2.0/Araba/ARABA/araba_kutuphane.h
+++ ARABA 2.0/Araba/ARABA/araba_kutuphane.h
@@ -11,23 +11,58 @@
   void right_stop();
   void all_stop();
   void motor_test();
-  int mesafe_olc();
-
-
-int mesafe_olc() {
-  
-  digitalWrite(pin_mesafe_trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pin_mesafe_trig, LOW);
-
-  unsigned long sure = pulseIn(pin_mesafe_echo, HIGH);
-  int mesafe_cm = sure * 0.034 / 2;
-  
-  return mesafe_cm;
-}
-
+ bool zaman_say_milis(unsigned int zaman_say_interval  );
+ bool zaman_say_micros(unsigned int zaman_say_interval  );
   void run_right( signed short speed);
   void run_left(  signed short speed);
+  void alarm(uint8_t mesafe);
+uint8_t mesafe_olc();
+bool zaman_say_milis(unsigned int zaman_say_interval  );
+bool zaman_say_micros(unsigned int zaman_say_interval  );
+
+
+void alarm(uint8_t mesafe){
+        digitalWrite(pin_far, HIGH);
+        digitalWrite(pin_korna, HIGH);
+        delay(4 * mesafe);      
+        digitalWrite(pin_far, LOW);
+        digitalWrite(pin_korna, LOW);
+        delay(4 * mesafe );      
+}
+
+uint8_t mesafe_olc(){
+  if( zaman_say_micros(500) ){
+    digitalWrite(pin_mesafe_trig, HIGH);
+    if( zaman_say_micros(10)  ) digitalWrite(pin_mesafe_trig, LOW);
+    zaman = pulseIn(pin_mesafe_echo, HIGH);
+    mesafe = (zaman / 29.1) / 2;
+    return mesafe;
+  }
+}
+
+
+ bool zaman_say_milis(unsigned int zaman_say_interval  ){
+        unsigned long currentTime = millis();
+        // İki sinyal arasındaki süreyi kontrol et
+        if (currentTime - baslangic_zamani_millis >= zaman_say_interval) {
+            baslangic_zamani_millis = currentTime; // Sonraki sinyal için süre sayacını sıfırla
+            return true;
+        }
+        return false;
+    }
+ bool zaman_say_micros(unsigned int zaman_say_interval  ) {
+        unsigned long currentTime = micros();
+        // İki sinyal arasındaki süreyi kontrol et
+        if (currentTime - baslangic_zamani_micros >= zaman_say_interval) {
+            baslangic_zamani_micros = currentTime; // Sonraki sinyal için süre sayacını sıfırla
+            return true;
+        }
+        return false;
+    }
+
+ 
+
+
 
   void run_right(signed short speed){
     if ( speed ==0  ) right_stop();
